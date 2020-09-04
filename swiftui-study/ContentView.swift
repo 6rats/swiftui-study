2c8326a31174fc7cdca538d9fd777e92486fb6c5
Takuro Murata <include.takuro@gmail.com>
2020-09-05 06:55:28 +0900

    Initial Commit

---
+++ swiftui-study/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-study
+//
+//  Created by murata on 2020/09/05.
+//  Copyright © 2020 Takuro Murata. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
